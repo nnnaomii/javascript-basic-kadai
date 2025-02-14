edcab6f4bff5276c9fb81c3b09db21d7b1c69d7e
nnnaomii <nekoko46@gmail.com>
2025-02-15 07:54:10 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 体重：68kg
+// 身長：1.7m
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+
+// 変数の宣言
+let weight /*体重*/
+let height /*身長*/
+let bmi /*BMI計算式*/
+
+// 値の代入
+weight = 68; /*体重*/ 
+height = 1.7; /*身長*/
+bmi = weight/(height*height);
+
+// 出力
+console.log("BMI:",bmi);
+
